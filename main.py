6def5b0ebd201398c20a2292f071cc8e2ad1ed3b
easilva-test-devops <edevalsilva@uol.com.br>
2026-09-08 22:48:21 -0300

    Adicionado submenu_1

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ print('(3) Gerenciar disciplinas.')
 print('(4) Gerenciar turmas.')
 print('(5) Gerenciar matrículas.')
 print('(9) Sair.\n')
+op = int(input('Informe a opção desejada: '))
+
+if op == 1:
+    print("\n\n***** [ESTUDANTES] MENU DE OPERAÇÕES *****\n")
+elif op == 2:
+    print("\n\n***** [PROFESSORES] MENU DE OPERAÇÕES *****\n")
+elif op == 3:
+    print("\n\n***** [DISCIPLINAS] MENU DE OPERAÇÕES*****\n")
+elif op == 4:
+    print("\n\n***** [TURMAS] MENU DE OPERAÇÕES *****\n")
+elif op == 5:
+    print("\n\n***** [MATRICULAS] MENU DE OPERAÇÕES *****\n")
+elif op == 9:
+    pass
+else:
+    print("\n\nOpção incorreta!")
+
